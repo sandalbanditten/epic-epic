2c5289eb3e9221f906779c80f4d963476353d1da
sandalbanditten <sandalbanditten@tutanota.com>
2022-05-17 14:46:56 +0200

    Updates canvas size on window resize

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
 	background(220);
+}
+
+function windowResized {
+	resizeCanvas(windowWidth, windowHeight);
 }
