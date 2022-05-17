06adfaca8f80a28f0002fec05e6bd2e6894c4734
gulledrengen <gustav.werk@gmail.com>
2022-05-17 15:18:36 +0200

    made blue circle for testing reasons (not final)
    yuh

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	projLine = new ProjLine()
 }
 
 function draw() {
 	background(220,0,0);
+<<<<<<< Updated upstream
 	showUi('balls', 'cock', width, height);
+=======
+
+	fill(0,0,255)
+	projLine.show()
+>>>>>>> Stashed changes
 }
 
 function windowResized() {
