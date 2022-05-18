3c8fd0abf44a00ca1fd7bf66735223e69c51022a
sandalbanditten <sandalbanditten@tutanota.com>
2022-05-18 09:45:37 +0200

    A little refactoring

--- src/playerClass.js
+++ src/playerClass.js
@@ -1,32 +1,32 @@
 class Player {
   constructor(width, height, speed, size, lives) {
-    this.size = size;
-    this.posX = width - this.size/2;
-    this.posY = height - this.size/2;
+    this.diameter = size;
+    this.posX = width - this.radius();
+    this.posY = height - this.radius();
     this.speed = speed;
     this.lives = lives;
   }
 
-  update() {  
-    circle(this.posX, this.posY, this.size);
+  update() {
+    circle(this.posX, this.posY, this.diameter);
     if(keyIsDown(87)) {
       this.posY -= this.speed;
     }
     if(keyIsDown(83)) {
       this.posY += this.speed;
     }
-      
+
     if(this.collisionCheck()) {
       this.lives--;
     }
     this.isDead();
 
-    this.posX = width - this.size/2;
-    this.posY = Math.min(Math.max(this.posY, 0 + this.size/2), height - this.size/2);
+    this.posX = width - this.radius();
+    this.posY = Math.min(Math.max(this.posY, 0 + this.radius()), height - this.size/2);
   }
 
   collisionCheck() {
-    if(dist(mouseX, mouseY, this.posX, this.posY) <= this.size/2) {
+    if(dist(mouseX, mouseY, this.posX, this.posY) <= this.radius()) {
       return true;
     } else {
       return false;
@@ -40,4 +40,8 @@ class Player {
       return false;
     }
   }
+
+	radius() {
+		return this.diameter / 2
+	}
 }
