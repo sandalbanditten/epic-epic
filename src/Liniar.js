71d4711b5f8bcfc55b7184ebc5a4d14a4871c624
gulledrengen <gustav.werk@gmail.com>
2022-05-18 12:38:10 +0200

    nword

--- src/Liniar.js
+++ src/Liniar.js
@@ -3,13 +3,13 @@ class Liniar extends Enemy
   constructor(size, type, speed)
   {
     super(size, type, speed);
+
+    this.a = (this.y1 - this.y2)/(this.x1 - this.x2);
+    this.b = this.y1;
   }
 
   update()
   {
-    this.a = (this.y1 - this.y2)/(this.x1 - this.x2);
-    this.b = this.y1;
-
     this.y = this.a * this.x + this.b;
     this.x += this.speed;
   }
